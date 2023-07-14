2d67912511f0412833166dad5da5213299949cf8
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-14 17:09:29 +0900

    [FEAT] 날씨 기본 틀 설정

--- yujinWeather/App.js
+++ yujinWeather/App.js
@@ -1,24 +1,57 @@
 // import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex:1}}>
-      <View style={{flex:1, backgroundColor:"tomato"}}></View>
-      <View style={{flex:1, backgroundColor:"orange"}}></View>
-      <View style={{flex:1, backgroundColor:"teal"}}></View>
-      <Text>
-        하이
-      </Text>
-    </View>
+      <View style={styles.container}>
+        <StatusBar style='light'></StatusBar>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <View style={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.desc}>Sunny</Text>
+          </View>
+
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor:"pink"
   },
+  city:{
+    flex:1,
+    backgroundColor: "white",
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  weather:{
+    flex:3,
+    backgroundColor: "purple"
+  },
+  day:{
+    alignItems:'center',
+    flex:1,
+    backgroundColor: "teal"
+  },
+  cityName:{
+    fontSize:68,
+    fontWeight:'500',
+    backgroundColor: "white"
+  },
+  temp:{
+    marginTop:50,
+    fontSize:160
+  },
+  desc:{
+    marginTop:-30,
+    fontSize:60
+  },
+
 });
