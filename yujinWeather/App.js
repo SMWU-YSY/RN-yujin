b65c3ea955ef02a602fcc6bcb13b6b2315cf29ea
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-14 17:44:55 +0900

    [FEAT] requestPermission, reverseGeocode

--- yujinWeather/App.js
+++ yujinWeather/App.js
@@ -1,16 +1,39 @@
 // import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import React from 'react';
 import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 const {width:SCREEN_WIDTH}=Dimensions.get("window");
 export default function App() {
+  const [days, setDays]= useState([]);
+  const [ok, setOk]= useState(true);
+  const [city, setCity]= useState("");
+  const getWeather=async()=>{
+    const {granted}= await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+    const {coords:{latitude,longitude}}=await Location.getCurrentPositionAsync({acuracy:1});
+    const location=await Location.reverseGeocodeAsync({latitude,longitude},{useGoogleMaps:false});
+    setCity(location[0].region);
+  };
+  useEffect(()=>{
+    getWeather();
+  },[]);
   return (
       <View style={styles.container}>
         <StatusBar style='light'></StatusBar>
         <View style={styles.city}>
-          <Text style={styles.cityName}>Seoul</Text>
+          <Text style={styles.cityName}>{city}</Text>
         </View>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false} pagingEnabled contentContainerStyle={styles.weather}>
+        <ScrollView 
+          horizontal 
+          showsHorizontalScrollIndicator={false} 
+          pagingEnabled 
+          contentContainerStyle={styles.weather}
+        >
           <View style={styles.day}>
             <Text style={styles.temp}>27</Text>
             <Text style={styles.desc}>Sunny</Text>
