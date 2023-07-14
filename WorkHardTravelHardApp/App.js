a6a628a87023476ec146e2358cc42a3d715e68c7
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-14 21:25:23 +0900

    [FEAT] Touchable, todo 세팅

---
+++ WorkHardTravelHardApp/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { theme } from './colors';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <Text style={styles.btnText}>Travel</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal:20,
+
+    // alignItems: 'center',///
+    // justifyContent: 'center',
+  },
+  header:{
+    flexDirection:"row",
+    marginTop:100,
+    justifyContent:'space-between'
+  },
+  btnText:{
+    fontSize:50,
+    fontWeight:'600',
+    color:theme.grey,
+  },
+});
