aa38112a1fd12d26e18240e815f5fbe15ad047c2
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-14 16:53:02 +0900

    [FEAT] 2.4 view와 flex

--- yujinWeather/App.js
+++ yujinWeather/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View style={{width:100, height:100, backgroundColor:"tomato"}}></View>
-      <View style={{width:100, height:100, backgroundColor:"orange"}}></View>
-      <View style={{width:100, height:100, backgroundColor:"teal"}}></View>
+    <View style={{flex:1}}>
+      <View style={{flex:1, backgroundColor:"tomato"}}></View>
+      <View style={{flex:1, backgroundColor:"orange"}}></View>
+      <View style={{flex:1, backgroundColor:"teal"}}></View>
       <Text>
         하이
       </Text>
