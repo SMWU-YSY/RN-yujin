284be62d98de1ebe033e6a155120448b432d3cd7
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-19 00:35:11 +0900

    [FEAT] Scrollview안에 view map

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import { theme } from './colors';
 import { useState } from 'react';
 
@@ -14,10 +14,15 @@ export default function App() {
     if(text===""){
       return;
     }
-    const newToDos=Object.assign(
-      {},
-      toDos,
-      {[Date.now()]:{text, work:working}})
+    const newToDos = {
+      ...toDos, 
+      [Date.now()] : { text, work: working },
+    };
+
+    // const newToDos=Object.assign(
+    //   {},
+    //   toDos,
+    //   {[Date.now()]:{text, work:working}})
     // alert(text);
     setToDos(newToDos);
     setText("");
@@ -42,6 +47,13 @@ export default function App() {
         style={styles.input}
         placeholder={working?"Add a To Do":"Where do you want to go?"}
       />
+      <ScrollView>
+        {Object.keys(toDos).map(key=><View style={styles.toDos} key={key}>
+          <Text style={styles.toDosText}>
+            {toDos[key].text}
+          </Text>
+        </View>)}
+      </ScrollView>
     </View>
   );
 }
@@ -66,7 +78,19 @@ const styles = StyleSheet.create({
     paddingVertical:15,
     paddingHorizontal:20,
     borderRadius:20,
-    marginTop:20,
+    marginVertical:20,
     fontSize:20,
   },
+  toDos:{
+    backgroundColor: theme.toDoBg,
+    marginBottom:10,
+    paddingVertical:20,
+    paddingHorizontal:20,
+    borderRadius:15,
+  },
+  toDosText:{
+    color:'white',
+    fontSize:16,
+    fontWeight:"500"
+  }
 });
